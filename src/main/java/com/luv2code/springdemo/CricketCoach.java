48fe4d1bd2b5f2f713970759dfd6a3df67ef9641
chengchengy <chengchengyang.cc@gmail.com>
2018-01-20 22:58:05 -0800

    Add another setter

--- src/main/java/com/luv2code/springdemo/CricketCoach.java
+++ src/main/java/com/luv2code/springdemo/CricketCoach.java
@@ -6,7 +6,29 @@ import com.luv2code.springdemo.FortuneService;
 public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
-	
+
+	//add new fields for emailAddress and team
+	private String emailAddress;
+	private String team;
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		System.out.println("CricketCoach: inside setter method - getTeam");
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
 	// create a no-arg constructor
 	public CricketCoach() {
 		System.out.println("CricketCoach: inside no-arg constructor");
--- src/main/java/com/luv2code/springdemo/SetterDemoApp.java
+++ src/main/java/com/luv2code/springdemo/SetterDemoApp.java
@@ -18,7 +18,10 @@ public class SetterDemoApp {
 		System.out.println(theCoach.getDailyWorkout());
 		
 		System.out.println(theCoach.getDailyFortune());
-						
+
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
+		//
 		// close the context
 		context.close();
 	}
